635ff2a868d9c2f35ff20a098cb8e0038f10e1cd
Gabriel Santana <gabrielsantana77@hotmail.com>
2021-04-02 19:18:15 -0300

    first commit

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'oemailqueforusar@email.com',
+      name: 'Gabriel',
+    },
+  },
+} as IMailConfig;
